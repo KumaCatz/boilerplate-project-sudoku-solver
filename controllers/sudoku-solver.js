95c96fe8ee8ef561b4b0d50f7d8791773808ff09
KumaCatz <tod.kagan@gmail.com>
2024-07-16 20:55:37 -0300

    feat: validating puzzle string

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,39 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    const match = /^[0-9.]+$/;
+
+    if (!puzzleString || puzzleString.length === 0) {
+      throw { error: 'Required field missing' };
+    } else if (!match.test(puzzleString)) {
+      throw { error: 'Invalid characters in puzzle' };
+    } else if (puzzleString.length !== 81) {
+      throw { error: 'Expected puzzle to be 81 characters long' };
+    }
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    // if (!isValid) {
+    //   throw {valid: false, conflict: 'row'}
+    // }
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    // if (!isValid) {
+    //   throw {valid: false, conflict: 'column'}
+    // }
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    // if (!isValid) {
+    //   throw {valid: false, conflict: 'region'}
+    // }
   }
 
   solve(puzzleString) {
-    
+    // if (!solution) {
+    //   throw {error: 'Puzzle cannot be solved' }
+    // }
   }
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,33 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route('/api/check').post((req, res) => {
+    const { puzzle, coordinate, value } = req.body;
+    const row = coordinate[1]
+    const column = coordinate[0]
+    try {
+      // solver.validate(puzzle)
+      console.log(req.body)
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+      solver.checkRowPlacement(puzzle, row, column, value)
+      solver.checkColPlacement(puzzle, row, column, value)
+      solver.checkRegionPlacement(puzzle, row, column, value)
+      return res.json({valid: true})
+    } catch(err) {
+      return res.json(err)
+    }
+  });
 
-    });
+  app.route('/api/solve').post((req, res) => {
+    const { puzzle } = req.body;
+    try {
+      solver.validate(puzzle)
+      solver.solve(puzzle)
+      return res.json({ solution: 'test' });
+    } catch(err) {
+      return res.json(err)
+    }
+  });
 };
